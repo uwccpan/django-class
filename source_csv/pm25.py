b47e427657923404957d9e436bad5f6dcc0c08f0
uwccpan <uwccpan@yahoo.com>
2021-10-01 14:17:07 +0800

    add code

--- source_csv/pm25.py
+++ source_csv/pm25.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-url = '/Users/jonpan/Downloads/aqx_p_322_20210930105441.csv'
+url = 'https://data.epa.gov.tw/api/v1/aqx_p_02?limit=1000&api_key=9be7b239-557b-4c10-9775-78cadfc555e9&sort=ImportDate%20desc&format=csv'
 
 def get_pm25():
     df = pd.read_csv(url).dropna()
